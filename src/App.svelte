80a07e2e58f68d4890204eb79b9346f45b9002ab
lazhenri <95055670+lazhenri@users.noreply.github.com>
2022-01-18 15:27:42 -0300

    atualização
    atualização

--- src/App.svelte
+++ src/App.svelte
@@ -1,61 +1,52 @@
-<script>
-	import { Router, Route, Link } from "svelte-navigator";
-	import Game from "./Game/Game.svelte";
-	import Sobre from "./Sobre/Sobre.svelte";
-</script>
+main {
+    padding: 10px;
+    margin: 10px;
+    border-radius: 5px;
+}
 
-<Router>
-	<header>
-		<h1>Campo Minado</h1>
-		<nav>
-			<Link class="link" to="/">Home</Link>
-			<Link class="link" to="game">Jogar</Link>
-			<Link class="link" to="sobre">Sobre</Link>
-		</nav>
-	</header>
-	<main>
-		<Route path="/">
-			<h3>Home</h3>
-			<p>Home sweet home...</p>
-		</Route>
+body {
+	background-color: #1a1a1a;
+	font-family: Arial, Helvetica, sans-serif;
+	color: #fff ;
+}
+h1 {
+	border: 2px solid #5fe36a;
+	background-color: #28348a;
+	text-transform: uppercase;
+	color: #fff;
+	border-radius: 5px;
+	text-align: center;
+}
+h2 {
+	color: #fff;
+  }
+h3 {
+	color: #fff;
+}
 
-		<Route path="game">
-			<Game/>
-		</Route>
+nav {
+    margin-left: 25%;
+}
 
-		<Route path="sobre">
-			<Sobre/>
-		</Route>
+.link {
+	transform-style: preserve-3d;
+	text-decoration: none;
+	text-transform: uppercase;
+	font-size: 20px;
 
-	</main>
-</Router>
+	border-radius: 5px;
+	font-weight: bold;
+	color: #fff;
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-	nav{
-		margin-left: 46%;
-	}
+	margin: 0px 15px;
+	padding: 10px 35px;
 
-	.link{
-		margin-left: 5%;
-	}
-	
-	h1 {
-		color: black;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-		margin-left: 37%;
-	}
+	border: 2px solid #5fe36a;
+}
+
 
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
-		}
 	}
-</style>
+}
